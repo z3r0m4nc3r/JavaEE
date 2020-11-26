30d380f78e72e3a50cb1737aa5e3fa016435f14b
z3r0m4nc3r <domingo.sd78@gmail.com>
2020-11-26 13:03:55 +0100

    26/11/2020

--- Spring17_formacion_spring_data_jpa_MVC/src/controller/AlumnoController.java
+++ Spring17_formacion_spring_data_jpa_MVC/src/controller/AlumnoController.java
@@ -3,7 +3,9 @@ package controller;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -16,19 +18,16 @@ public class AlumnoController {
 	AlumnosService service;
 
 	@PostMapping("doAltaAlumno")
-	public String altaAlumno(@RequestParam("usuario") String usuario,
-			@RequestParam("email") String email,
-			@RequestParam("password") String password,
-			@RequestParam("nombre") String nombre,
-			@RequestParam("edad") Integer edad) {
+	public String altaAlumno(@ModelAttribute("alumno") Alumno alumno) {
 		
-		if(service.buscarAlumnoPorUsuario(usuario)==null) {
-			Alumno alumno=new Alumno(usuario, edad, email, nombre, password);
+		if(service.buscarAlumnoPorUsuario(alumno.getUsuario())==null) {
+			
 			service.altaAlumno(alumno);
 			return "../menu";
 	}else return "../menu";
 
-}
+	}
+	
 	@GetMapping("doAlumnos")
 	public String listaAlumnos(HttpServletRequest request) {
 		request.setAttribute("alumnos", service.alumnos());
